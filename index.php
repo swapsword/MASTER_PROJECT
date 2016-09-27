3378104496c8c093cd28d5b739ac7b733d21eed9
swapsword <swapnilpathak1989@gmail.com>
2016-09-27 10:41:53 +0200

    modified index.php with html only as php is already added into main file

--- index.php
+++ index.php
@@ -1,28 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <style>
+        body{
+            background-color: #55acee;
+        }
+        h2{
+            color: #ffffff;
+        }
+        h1{
+            color: #55acee;
+            font-family: "Courier New";
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <h1>Tweets On Map</h1>
+                <a class="navbar-brand" href="#"></a>
+            </div>
+        </div>
+    </nav>
 
-require_once './config/config.php';
-
-
-$query ="q={$_REQUEST['q']}";//&place={$_REQUEST['place']}";
-
-$reply = $cb->search_tweets($query, true);
-
-//debug_arr($reply);
-
-$data = array();
-foreach($reply->statuses as $status){
-  $data[] = array(
-      'text' => $status->text,
-      'location' => $status->user->location,
-      'screen_name' => $status->user->screen_name,
-  );
-
-}
-$dataf= json_encode($data);
-//debug_arr($dataf);
-//debug_arr(var dataa = $data;);
-echo "var php_var = $dataf;";
-//debug_arr($data, false);
-//include_once './views/home.php';
-?>
+    <div class="container">
+        <h2>Search Tweets</h2>
+        <form method="post" action="method.php"class="form-inline">
+            <input type="text" name="search" class="form-control input-lg"placeholder="Enter Search" class="box-shadow">
+            <button type="submit" class="btn btn-default btn-lg active">Search</button>
+        </form>
+    </div>
+    <div class="row">
+        <div class="col-xs-6 col-md-4"></div>
+        <div class="col-xs-6 col-md-4"></div>
+        <div class="col-xs-6 col-md-4"></div>
+    </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
 
